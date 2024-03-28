2bfc9b811b2ae9b182684ccacd51d43fe6b64515
Davidbermu123 <david.bermudez01@usa.edu.co>
2024-03-28 16:51:02 -0500

    primeros cambios

--- Desmotivados/src/main/resources/static/vistas/main.js
+++ Desmotivados/src/main/resources/static/vistas/main.js
@@ -5,19 +5,27 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
+        const selectedTask = document.getElementById('select-task').value;
         const taskName = document.getElementById('task-name').value;
         const taskDate = document.getElementById('task-date').value;
         const taskTime = document.getElementById('task-time').value;
         const priority = document.getElementById('priority').value;
 
-        if (taskName && taskDate && taskTime && priority) {
-            message.innerText = 'La tarea fue correctamente editada.';
+        if (selectedTask && taskName && taskDate && taskTime && priority) {
+            // Muestra el mensaje de confirmación de la tarea seleccionada
+            message.innerText = `La tarea "${selectedTask}" fue modificada.`;
             message.classList.remove('hidden');
+
+            // Muestra el mensaje de éxito
+            setTimeout(function() {
+                message.innerText = 'Tarea modificada exitosamente.';
+            }, 2000); // Muestra el mensaje de éxito después de 2 segundos
+
+            // Limpia el formulario
+            form.reset();
         } else {
-            message.innerText = 'Por favor, llenar todos los espacios.';
+            message.innerText = 'Por favor, llena todos los espacios.';
             message.classList.remove('hidden');
         }
-
-        // Puedes agregar aquí más lógica según tus necesidades
     });
 });
