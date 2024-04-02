6cf8caac6da13832f5e847139261b6a766b64535
Davidbermu123 <david.bermudez01@usa.edu.co>
2024-04-02 01:15:27 -0500

    casi acabado

--- Desmotivados/src/main/java/com/Proyectousa/Desmotivados/Controladores/TareasController.java
+++ Desmotivados/src/main/java/com/Proyectousa/Desmotivados/Controladores/TareasController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,31 +25,14 @@ public class TareasController {
     public List<Tareas> getAllTareas() {
         return tareasService.findTodo();
     }
-/* 
-    @GetMapping("/modificarTarea/{id}")
-    public ResponseEntity<Tareas> obtenerTarea(@PathVariable Long id) {
-        Tareas tarea = tareasService.obtenerTareaPorId(id);
-        if (tarea != null) {
-            return new ResponseEntity<>(tarea, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    
+    @PostMapping("/modificarTarea")
+    public ResponseEntity<String> modificarTarea(@RequestBody Tareas tarea) {
+        try {
+            tareasService.modificarTarea(tarea);
+            return new ResponseEntity<>("Tarea modificada correctamente", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al modificar la tarea: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-    @PostMapping("/actualizarTarea/{id}")
-    public ResponseEntity<Tareas> actualizarTarea(@PathVariable Long id, @RequestBody Tareas tarea) {
-        Tareas tareaExistente = tareasService.obtenerTareaPorId(id);
-        if (tareaExistente != null) {
-            tareaExistente.setTitulo(tarea.getTitulo());
-            tareaExistente.setDescripcion(tarea.getDescripcion());
-            tareaExistente.setFechaInicio(tarea.getFechaInicio());
-            tareaExistente.setFechaFinal(tarea.getFechaFinal());
-            tareaExistente.setPrioridad(tarea.getPrioridad());
-            Tareas tareaActualizada = tareasService.actualizarTarea(tareaExistente);
-            return new ResponseEntity<>(tareaActualizada, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-    */
 }
